49b48aff29de9f5cf58c0f3f37225e2170ccaf6c
HenryFerreira <henryferreira133@gmail.com>
2022-02-16 20:16:30 -0300

    Commit inicial del programa

---
+++ src/main/java/com/sofka/spring_boot/SpringBoot_Sofka/SpringBootSofkaApplication.java
@@ -0,0 +1,13 @@
+package com.sofka.spring_boot.SpringBoot_Sofka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSofkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSofkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sofka/spring_boot/SpringBoot_Sofka/SpringBootSofkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofka.spring_boot.SpringBoot_Sofka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSofkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
